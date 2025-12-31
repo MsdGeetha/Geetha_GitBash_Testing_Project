1730426356fffdc758c76721d86343962792f8cc
MsdGeetha <geethalolekar@gmail.com>
2025-12-31 16:22:03 +0530

    Framework phase is ready

---
+++ src/test/java/gitBash_Testing/Testing_Git_Bash.java
@@ -0,0 +1,11 @@
+package gitBash_Testing;
+
+import org.testng.annotations.Test;
+
+public class Testing_Git_Bash {
+	@Test
+	public void test() {
+		System.out.println("Git Bash Testing");
+	}
+	
+}
