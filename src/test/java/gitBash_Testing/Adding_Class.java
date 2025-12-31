0985cc5ad89c26ec2e1a0b209000b59e010c27e0
MsdGeetha <geethalolekar@gmail.com>
2025-12-31 16:26:01 +0530

    adding the extra class

---
+++ src/test/java/gitBash_Testing/Adding_Class.java
@@ -0,0 +1,11 @@
+package gitBash_Testing;
+
+import org.testng.annotations.Test;
+
+public class Adding_Class {
+	@Test
+	public void test() {
+		System.out.println("Adding the extra class for testing");
+	}
+	
+}
